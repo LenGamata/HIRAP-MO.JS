d2bc4d07f08bc5f6366965b8881ca3b0de15fed0
LenGamata <lengamata14@gmail.com>
2024-10-31 17:41:15 +0700

    Update User.js

--- User.js
+++ User.js
@@ -1,10 +1,1 @@
-// Create the userProfile object
-const userProfile = {
-    fullName: 'John Doe',        // Replace with the user's full name
-    age: 25,                     // Replace with the user's age
-    favoriteNumber: 7,          // Replace with the user's favorite number
-    favoriteColors: ['blue', 'green', 'red'] // Replace with the user's favorite colors
-};
 
-// Log the entire object to the console
-console.log(userProfile);
